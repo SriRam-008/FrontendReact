18a9d10d318f9cf9481d401b57dcbe6106318bc5
SriRam-008 <sriramcse1205@gmail.com>
2025-02-21 11:25:49 +0530

    Modified Content.jsx in frontendreact

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import {useState} from 'react'
 
 const Content = () => {
-    // const greetlist = ['hello','hi','hola','namaste'];
-    // const rand = greetlist[Math.floor(Math.random()*10)];
-    // const greet = greetlist[rand]
+
+    const greetlist = ['Hello','Hi','Hola','Namaste'];
+    const rand = Math.floor((Math.random()*4));
+    const greet = greetlist[rand];
     
     // const [count,setcount] = useState(0);
     // const [greet,setgreet] = useState("Hello");
@@ -14,25 +15,32 @@ const Content = () => {
     //     else if (count === 1)
     //     setgreet("Namaste");
     // }
-    const [color,setColor] = useState('');
-    const [count,setCount] = useState(0);
-    const handleClick =() =>{
-      setCount(count+1);
-    }
+
+    
+    // const [color,setColor] = useState('');
+    // const [count,setCount] = useState(0);
+    // const handleClick =() =>{
+    //   setCount(count+1);
+    // }
     
    
   return (
-    <div style={{backgroundColor:color}}>
+    // <div style={{backgroundColor:color}}>
+    <div>
         <h3>
+            {greet} students, welcome all!
+        </h3>
+        {/* <h3>
             {count} times clicked
         </h3>
+        
         <button onClick={handleClick}>Clickme</button>
         <br>
         </br>
         <input type='text' placeholder='Enter Color' onChange={(e) =>{
           setColor(e.target.value);
           console.log("e.target.value")
-        }}></input>
+        }}></input> */}
     </div>
   )
 }
